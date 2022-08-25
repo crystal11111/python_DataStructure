fbebe9ccc0c2cfbfcb5d919e344ec9250733ae19
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-25 15:12:40 +0900

    Update Day08_ex6.py

--- Day8/Day08_ex6.py
+++ Day8/Day08_ex6.py
@@ -17,8 +17,7 @@ class Queue:
     # rear가 front보다 한 칸 전에 있으면: rear == front - 1
     # front가 rear보다 한 칸 뒤에 있으면: front == rear + 1
     def isFull(self):
-        if self.front == self.size - 1:
-            self.rear + 1:
+        if self.front == (self.rear+ 1) % self.size:
             return True
         else:
             return False
@@ -26,19 +25,43 @@ class Queue:
     # item을 rear(파란 화살표 자리)에 넣어 주는 거
     def enqueue(self, item):
         self.queue[self.rear] = item
-        self.rear += 1                  # rear가 한 칸 전진
+        self.rear = (self.rear + 1) % self.size         # rear가 한 칸 전진
 
     # front(빨간 화살표 자리)값을 제거하는 거
-    def dequeue(self, item):
+    def dequeue(self):
         self.queue[self.front] = [None]
-        self.front += 1                 # front가 한 칸 전진
+        self.front = (self.front + 1) % self.size      # front가 한 칸 전진
 
+    def display(self):  # front ~ rear -1
+        index = self.front
+        while index <= self.rear - 1:
+            print(self.queue[index], end = " ")
+            index += 1
+        #print(self.queue[self.front:self.rear])
+        print()
+
+q = Queue()
+q.enqueue(3)
+q.enqueue(5)
+q.enqueue(7)
+q.enqueue(1)
+q.display()
+
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.display()
+
+print(q.isFull())
+q.dequeue()
+print(q.isEmpty())
 
 """
 다른 언어에서는,
 None이라는 용어 자체가 없다
 = 변수 안에 값을 제거하는 거 자체가 없다
 
-
+그래서 자료구조를 표현(= 값이 제거됐다)
+저료구조의 범위(ex. front ~ rear -1)
 
 """
